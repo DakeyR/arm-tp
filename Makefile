a9a098382f8ea3baa87a7c69e401b129dbf24385
ThinkChaos <ThinkChaos@users.noreply.github.com>
2018-05-14 19:47:09 +0200

    start.c: Init .bss and .data

--- Makefile
+++ Makefile
@@ -10,12 +10,14 @@ OBJS = $(SRC:.c=.o)
 CFLAGS = -ffunction-sections  -mcpu=cortex-m4 -nostartfiles -g #, -gc-sections -Map=obj.map
 LDFLAGS = -T stm32.lds -gc-sections
 
+
 .PHONY: all clean
 
 all: stm32.bin
 
 clean:
 	$(RM) $(OBJS) stm32.out
+
 
 stm32.elf: $(OBJS) stm32.lds
 	$(LD) $(LDFLAGS) $(OBJS) -o $@
--- src/start.c
+++ src/start.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 extern const void *_stack_start;
 
 void start(void);
@@ -10,11 +12,20 @@ const void *_isr_vector[82] __attribute__((section (".isr_vector"))) = {
 };
 
 
+extern uint32_t _bss_start, _bss_end, _data_start, _data_end, _data_loadaddr;
 extern void main(void);
 
 void start(void) {
-    // TODO: memcpy(_data_start, _data_loadaddr, _data_end - _data_end);
-    // TODO: memset(_bss_start, 0, _bss_end - _bss);
+    // memset(_bss_start, 0, _bss_end - _bss);
+    for (uint32_t *bss = &_bss_start; bss < &_bss_end; bss++)
+        *bss = 0;
+
+    // memcpy(_data_start, _data_loadaddr, _data_end - _data_end);
+    uint32_t *data_src = &_data_loadaddr;
+    uint32_t *data_dst = &_data_loadaddr;
+    while (data_src < &_data_loadaddr + ((int)&_data_end - (int)&_data_start))
+        *(data_dst++) = *(data_src++);
+
 
     main();
 
