023cf3916acfd8fe7f90710425fac04f63303ee5
ThinkChaos <ThinkChaos@users.noreply.github.com>
2018-05-17 20:51:53 +0200

    Turn on LED3

--- src/start.c
+++ src/start.c
@@ -15,12 +15,13 @@ const void *_isr_vector[82] __attribute__((section (".isr_vector"))) = {
 extern uint32_t _bss_start, _bss_end, _data_start, _data_end, _data_loadaddr;
 extern void main(void);
 
-void start(void) {
-    // memset(_bss_start, 0, _bss_end - _bss);
+void start(void)
+{
+    // memset(_bss_start, 0, _bss_end - _bss_start);
     for (uint32_t *bss = &_bss_start; bss < &_bss_end; bss++)
         *bss = 0;
 
-    // memcpy(_data_start, _data_loadaddr, _data_end - _data_end);
+    // memcpy(_data_start, _data_loadaddr, _data_end - _data_start);
     uint32_t *data_src = &_data_loadaddr;
     uint32_t *data_dst = &_data_start;
     while (data_src < &_data_loadaddr + ((int)&_data_end - (int)&_data_start))
