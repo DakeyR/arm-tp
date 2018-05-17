074816e59fbb3d1023e7e4263ba3af3e2070dc5f
Raphael Gault <raphael.gault@epita.fr>
2018-05-17 19:15:31 +0200

    Add code for GPIO register reading

--- src/main.c
+++ src/main.c
@@ -1,4 +1,19 @@
-void main(void) {
+#include <stdint.h>
+
+#define GET_REGISTER_ADDR(base, offset) base + offset
+#define GPIOA_BASE 0x40020000
+#define IDR_OFFSET 0x10
+
+void button_pressed(void)
+{
     while (1)
         ;
+}
+
+void main(void) {
+
+    uint32_t reg = GET_REGISTER_ADDR(GPIOA_BASE, IDR_OFFSET);
+    while (1)
+        if (reg & 0x0001)
+            button_pressed();
 }
