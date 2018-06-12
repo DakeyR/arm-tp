7e4973b59680dc8daec0a19eacbf0aac5a2c80cf
ThinkChaos <ThinkChaos@users.noreply.github.com>
2018-06-12 16:43:03 +0200

    main: Hello World over USART1

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 #include "board.h"
 #include "interrupt_handlers.h"
+#include "uart.h"
 
 
 void button_pressed(irq_num_t n)
@@ -12,6 +13,19 @@ void button_pressed(irq_num_t n)
 
 void main(void)
 {
+    REG_SET_BIT(RCC_APB2ENR, 4); // USART1 clock enable
+
+    // GET_REG(USART1_BRR) = (/* mantissa */ 8 << 4) | (/* frac */ 11); // 115200 bps
+    GET_REG(USART1_BRR) = (/* mantissa */ 416 << 4) | (/* frac */ 11); // 9600 bps
+
+    REG_SET_BIT(USART1_CR1, 13); // USART1 enable
+
+    const char buf[] = "Hello, World!\r\n";
+    uart_send(buf, sizeof(buf));
+
+    while (1)
+        continue;
+
     // TODO: Generic interrupt enabling
     REG_CLR_BIT(SYSCFG_EXTICR1, 0);
     REG_SET_BIT(EXTI_IMR, 0);
@@ -27,8 +41,6 @@ void main(void)
     GPIO_SET_PIN_MODE(G, 13, GPIO_PIN_MODE_OUTPUT);
 
     on_interrupt(6, button_pressed);
-
-    GET_REG(RCC_APB2ENR) |= 0x10;
 
     while (1)
         continue;
